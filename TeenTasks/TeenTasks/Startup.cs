3cf354e6b2e407bdad2b6e7415c3296929b30425
Erik Ralston <erik.ralston@gmail.com>
2016-06-29 10:07:57 -0700

    Initial project creation

---
+++ TeenTasks/TeenTasks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeenTasks.Startup))]
+namespace TeenTasks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
